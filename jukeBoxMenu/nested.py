0451c75700ade0e53ac93e93b46d781f0e08e0ae
Oskan1 <100508408+Oskan1@users.noreply.github.com>
2022-02-28 03:09:26 +0100

    Add files via upload

---
+++ jukeBoxMenu/nested.py
@@ -0,0 +1,60 @@
+albums = [
+    ("Welcome to my Nightmare", "Alice Cooper", 1975,
+     [
+         (1, "Welcome to my Nightmare"),
+         (2, "Devil's Food"),
+         (3, "The Black Widow"),
+         (4, "Some Folks"),
+         (5, "Only Women Bleed"),
+     ]
+     ),
+    ("Bad Company", "Bad Company", 1974,
+     [
+         (1, "Can't Get Enough"),
+         (2, "Rock Steady"),
+         (3, "Ready for Love"),
+         (4, "Don't Let Me Down"),
+         (5, "Bad Company"),
+         (6, "The Way I Choose"),
+         (7, "Movin' On"),
+         (8, "Seagull"),
+     ]
+     ),
+    ("Nightflight", "Budgie", 1981,
+     [
+         (1, "I Turned to Stone"),
+         (2, "Keeping a Rendezvous"),
+         (3, "Reaper of the Glory"),
+         (4, "She Used Me Up"),
+     ]
+     ),
+    ("More Mayhem", "Imelda May", 2011,
+     [
+         (1, "Pulling the Rug"),
+         (2, "Psycho"),
+         (3, "Mayhem"),
+         (4, "Kentish Town Waltz"),
+     ]
+     ),
+]
+
+#album = albums[3] # equal to 4th album in list
+#songs = album[3] # 4th option in album (list of songs)
+#song = songs[1] # 2nd song in list of songs
+#name_song = song[1] # 2nd option in tuple in list of songs
+#print(name_song)
+
+# or in 1 line
+#Psycho = albums[3][3][1][1]
+#print(Psycho)
+# easy way of accessing
+#print(albums[3][3]) # we print list of songs
+
+#print()
+#for name, artist, year, songs in albums:
+   # print("Album: {}".format(name))
+  #  print("Artist: {}".format(artist))
+  #  print("Publication Year: {}".format(year))
+   # print("Songs: {}".format(songs))
+  #  print()
+
